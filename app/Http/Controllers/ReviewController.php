c7f51f2b2862c56f82d45b5fdd6a3a0cffdac955
ykiyoshima <y.kiyoshima.271@gmail.com>
2022-07-10 14:33:11 +0900

    [fix] fixing skip bug

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -79,7 +79,11 @@ class ReviewController extends Controller
         for ($i = 1; $i <= 5; $i++) {
             if ($request['speaker_number'] == $i) {
                 if ($request->has('skip_btn')) {
-                    $request = null;
+                    return view('review.create', [
+                        'season' => Season::getSeasonBySeasonDate(date('Y-m-d')),
+                        'speaker_number' => (int)$request->$speaker_number,
+                        'reviewer_name' => $request->reviewer_name
+                    ]);
                 }
             }
         }
